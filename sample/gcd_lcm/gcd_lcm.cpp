a0248fa617a6ec6cb9c36b7c04123b579b202f3e
zyuzhi <tearsxy@gmail.com>
2017-02-27 22:49:16 +0800

    add sample of gcd_lcm

--- sample/gcd_lcm/gcd_lcm.cpp
+++ sample/gcd_lcm/gcd_lcm.cpp
@@ -5,20 +5,29 @@
 
 int main()
 {
-	using namespace zyuzhi::math;
-	// array
-	int arr[3] = {35, 63, 7};
-	std::cout << lcm(arr) << std::endl;
+    using namespace zyuzhi::math;
+    int arr[3] = {35, 63, 7};
+    std::vector<int> v;
+    for (int i = 0; i < 3; ++i) {
+        v.push_back(arr[i]);
+    }
+    // array lcm
+    std::cout << "lcm(35, 63, 7) = " << lcm(arr) << std::endl;
 
-	// vector
-	std::vector<int> v;
-	v.push_back(35);
-	v.push_back(63);
-	v.push_back(7);
-	std::cout << lcm(v) << std::endl;
+    // vector lcm
+    std::cout << "lcm(35, 63, 7) = " << lcm(v) << std::endl;
 
-	// meta template
-	std::cout << mpl::lcm<mpl::lcm<35, 63>::value, 7>::value << std::endl;
+    // meta template lcm
+    std::cout << "lcm(35, 63, 7) = " << mpl::lcm<mpl::lcm<35, 63>::value, 7>::value << std::endl;
 
-	return 0;
+    // array gcd
+    std::cout << "gcd(35, 63, 7) = " << gcd(arr) << std::endl;
+
+    // vector gcd
+    std::cout << "gcd(35, 63, 7) = " << gcd(v) << std::endl;
+
+    // meta template gcd
+    std::cout << "gcd(35, 63, 7) = " << mpl::gcd<mpl::gcd<35, 63>::value, 7>::value << std::endl;
+
+    return 0;
 }

dad6eecaa08d933ae7df2f47e2fc9e3a9d5a01f9
zyuzhi <tearsxy@gmail.com>
2017-02-27 23:12:32 +0800

    add macro file

---
+++ include/zyuzhi/math/macros.h
@@ -0,0 +1,18 @@
+// @author zyuzhi
+#pragma once
+#ifndef ZYUZHI_MATH_MACROS_H
+#define ZYUZHI_MATH_MACROS_H
+
+#ifndef ROOT_NAMESPACE_BEGIN
+#define ROOT_NAMESPACE_BEGIN namespace zyuzhi {
+#endif
+#ifndef ROOT_NAMESPACE_END
+#define ROOT_NAMESPACE_END }
+#endif
+#ifndef MATH_NAMESPACE_BEGIN
+#define MATH_NAMESPACE_BEGIN namespace math {
+#endif
+#ifndef MATH_NAMESPACE_END
+#define MATH_NAMESPACE_END }
+#endif
+#endif
